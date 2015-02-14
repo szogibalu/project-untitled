229121d32d5aee1b20c3ae82293970112ad051af
szogi <szogibalu@gmail.com>
2015-02-14 14:12:29 +0100

    Routes refactor

--- user-management-data-service/routes/users.js
+++ user-management-data-service/routes/users.js
@@ -4,44 +4,41 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = require('../models/user');
 
-router.get('/', function(req, res, next) {
-	User.find(function(err, user) {
-		if (err)
-			return next(err);
-		res.json(user);
+function setResponse(response, error, result){
+	if (error) {
+		return next(error);
+	}
+	response.json(result);
+};
+
+router.get('/', function(request, response, next) {
+	User.find(function(error, result) {	
+		setResponse(response, error, result);
 	});
 });
 
-router.post('/', function(req, res, next) {
-	User.create(req.body, function(err, post) {
-		if (err)
-			return next(err);
-		res.json(post);
+router.put('/', function(request, response, next) {
+	User.create(req.body, function(error, result) {
+		setResponse(response, error, result);
 	});
 });
 
-router.get('/:id', function(req, res, next) {
-	User.findById(req.params.id, function(err, post) {
-		if (err)
-			return next(err);
-		res.json(post);
+router.get('/:id', function(request, response, next) {
+	User.findById(req.params.id, function(error, result) {
+		setResponse(response, error, result);
 	});
 });
 
-router.put('/:id', function(req, res, next) {
-	User.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
-		if (err)
-			return next(err);
-		res.json(post);
+router.post('/:id', function(request, response, next) {
+	User.findByIdAndUpdate(req.params.id, req.body, function(error, result) {
+		setResponse(response, error, result);
 	});
 });
 
-router.delete('/:id', function(req, res, next) {
-  User.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-    if (err) 
-    	return next(err);
-    res.json(post);
-  });
+router.delete('/:id', function(request, response, next) {
+	User.findByIdAndRemove(req.params.id, req.body, function (error, result) {
+		setResponse(response, error, result);
+	});
 });
 
 module.exports = router;
