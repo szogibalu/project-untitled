cc4ea45ed594a7e53ec2140bbdd1cce12d1f7000
szogi <szogibalu@gmail.com>
2015-02-14 20:00:00 +0100

    Introduced user-service

--- user-management-data-service/routes/users.js
+++ user-management-data-service/routes/users.js
@@ -1,44 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('mongoose');
-var User = require('../models/user');
+var service = require('../services/user-service');
 
-function setResponse(response, error, result){
-	if (error) {
-		return next(error);
-	}
-	response.json(result);
-};
+router.get('/', service.getUsers);
 
-router.get('/', function(request, response, next) {
-	User.find(function(error, result) {	
-		setResponse(response, error, result);
-	});
-});
+router.put('/', service.addUser);
 
-router.put('/', function(request, response, next) {
-	User.create(req.body, function(error, result) {
-		setResponse(response, error, result);
-	});
-});
+router.get('/:id', service.getUser);
 
-router.get('/:id', function(request, response, next) {
-	User.findById(req.params.id, function(error, result) {
-		setResponse(response, error, result);
-	});
-});
+router.post('/:id', service.updateUser);
 
-router.post('/:id', function(request, response, next) {
-	User.findByIdAndUpdate(req.params.id, req.body, function(error, result) {
-		setResponse(response, error, result);
-	});
-});
-
-router.delete('/:id', function(request, response, next) {
-	User.findByIdAndRemove(req.params.id, req.body, function (error, result) {
-		setResponse(response, error, result);
-	});
-});
+router.delete('/:id', service.deleteUser);
 
 module.exports = router;
---
+++ user-management-data-service/services/user-service.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var User = require('../models/user');
+
+var service = {
+	"addUser" : function addUser(request, response, next) {
+		User.create(request.body, function(error, result) {
+			setResponse(response, error, result);
+		});
+	},
+	"getUser" : function getUser(request, response, next) {
+		User.findById(request.params.id, function(error, result) {
+			setResponse(response, error, result);
+		});
+	},
+	"getUsers" : function getUsers(request, response, next) {
+		User.find(function(error, result) {
+			setResponse(response, error, result);
+		});
+	},
+	"updateUser" : function updateUser(request, response, next) {
+		User.findByIdAndUpdate(request.params.id, request.body, function(error,
+				result) {
+			setResponse(response, error, result);
+		});
+	},
+	"deleteUser" : function deleteUser(request, response, next) {
+		User.findByIdAndRemove(request.params.id, request.body, function(error,
+				result) {
+			setResponse(response, error, result);
+		});
+	}
+}
+
+function setResponse(response, error, result) {
+	if (error) {
+		return next(error);
+	}
+	response.json(result);
+}
+
+module.exports = service;
