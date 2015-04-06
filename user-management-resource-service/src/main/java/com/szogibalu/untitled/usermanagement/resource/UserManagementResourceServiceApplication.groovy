5781dae7df955db5adaeea9508447059d3c0441c
szogi <szogibalu@gmail.com>
2015-04-06 15:40:48 +0200

    Added Resource Service

---
+++ user-management-resource-service/src/main/java/com/szogibalu/untitled/usermanagement/resource/UserManagementResourceServiceApplication.groovy
@@ -0,0 +1,12 @@
+package com.szogibalu.untitled.usermanagement.resource
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class UserManagementResourceServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run UserManagementResourceServiceApplication, args
+    }
+}
---
+++ user-management-resource-service/src/test/java/com/szogibalu/untitled/usermanagement/resource/UserManagementResourceServiceApplicationTests.groovy
@@ -0,0 +1,18 @@
+package com.szogibalu.untitled.usermanagement.resource
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = UserManagementResourceServiceApplication)
+@WebAppConfiguration
+class UserManagementResourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
