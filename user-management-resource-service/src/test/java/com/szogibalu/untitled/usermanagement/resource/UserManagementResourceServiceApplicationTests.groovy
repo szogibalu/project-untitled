fcd44cfa150b7bd46be899b6a13a3ee6a9207598
szogi <szogibalu@gmail.com>
2015-04-26 11:57:12 +0200

    Fixed tests

--- user-management-resource-service/src/test/java/com/szogibalu/untitled/usermanagement/resource/UserManagementResourceServiceApplicationTests.groovy
+++ user-management-resource-service/src/test/java/com/szogibalu/untitled/usermanagement/resource/UserManagementResourceServiceApplicationTests.groovy
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import redis.embedded.RedisServer
 
 @RunWith(SpringJUnit4ClassRunner)
 @SpringApplicationConfiguration(classes = UserManagementResourceServiceApplication)
