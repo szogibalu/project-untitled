6ece075c8f5a1212e3448f6ee4defe5492213249
szogi <szogibalu@gmail.com>
2015-04-27 21:44:24 +0200

    Enable OAuth2 in Resource Server

--- user-management-resource-service/src/main/java/com/szogibalu/untitled/usermanagement/resource/UserManagementResourceServiceApplication.groovy
+++ user-management-resource-service/src/main/java/com/szogibalu/untitled/usermanagement/resource/UserManagementResourceServiceApplication.groovy
@@ -2,9 +2,7 @@ package com.szogibalu.untitled.usermanagement.resource
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,18 +10,12 @@ import static java.util.UUID.randomUUID
 
 @SpringBootApplication
 @RestController
-@EnableRedisHttpSession
-class UserManagementResourceServiceApplication extends WebSecurityConfigurerAdapter {
+@EnableOAuth2Resource
+class UserManagementResourceServiceApplication {
 
     @RequestMapping('/')
     def home() {
         [id: randomUUID().toString(), message: 'Hello World']
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable()
-        http.authorizeRequests().anyRequest().authenticated()
     }
 
     static void main(String[] args) {
--- user-management-resource-service/src/test/java/com/szogibalu/untitled/usermanagement/resource/UserManagementResourceServiceApplicationTests.groovy
+++ user-management-resource-service/src/test/java/com/szogibalu/untitled/usermanagement/resource/UserManagementResourceServiceApplicationTests.groovy
@@ -1,40 +1,17 @@
 package com.szogibalu.untitled.usermanagement.resource
 
-import org.junit.AfterClass
-import org.junit.BeforeClass
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
-import redis.embedded.RedisServer
 
 @RunWith(SpringJUnit4ClassRunner)
 @SpringApplicationConfiguration(classes = UserManagementResourceServiceApplication)
 @WebAppConfiguration
 class UserManagementResourceServiceApplicationTests {
 
-    private static RedisServer redisServer;
-
-    @BeforeClass
-    public static void setup() throws Exception {
-        redisServer = new RedisServer(6379);
-        redisServer.start();
-    }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        try {
-            redisServer.stop();
-        } finally {
-            redisServer = null;
-        }
-    }
-
-
     @Test
-    @Ignore("Setup Redis at runtime")
     void contextLoads() {
     }
 
