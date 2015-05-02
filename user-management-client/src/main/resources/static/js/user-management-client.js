14abc1f6912939707582b148c70f2c203ed39eea
szogi <szogibalu@gmail.com>
2015-05-02 18:28:22 +0200

    Fixed Client module

--- user-management-client/src/main/resources/static/js/user-management-client.js
+++ user-management-client/src/main/resources/static/js/user-management-client.js
@@ -1,85 +1,23 @@
 'use strict';
 
-angular.module('user-management-client', ['ngRoute'])
-  .config(function($routeProvider, $httpProvider) {
+angular.module('user-management-client', []).controller('home',
 
-    $routeProvider.when('/', {
-      templateUrl: 'home.html',
-      controller: 'home'
-    }).when('/login', {
-      templateUrl: 'login.html',
-      controller: 'navigation'
-    }).otherwise('/');
+function($scope, $http) {
 
-    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	console.log('Loading')
 
-    $httpProvider.interceptors.push(function() {
-      return {
-        'request': function(config) {
-          return config;
-        },
-      }
-    });
+	$http.get('user').success(function(data) {
+		if (data.name) {
+			$scope.authenticated = true;
+			$scope.user = data.name
+			$http.get('/resource/').success(function(data) {
+				$scope.data = data;
+			})
+		} else {
+			$scope.authenticated = false;
+		}
+	}).error(function() {
+		$scope.authenticated = false;
+	});
 
-  })
-  .controller('home', function($scope, $http) {
-    $http.get('token').success(function(token) {
-      $http({
-        url: 'resource/',
-        method: 'GET',
-        headers: {
-          'X-Auth-Token': token.token
-        }
-      }).success(function(data) {
-        $scope.data = data.message;
-      });
-    })
-  })
-  .controller('navigation', function($rootScope, $scope, $http, $location) {
-
-    var authenticate = function(credentials, callback) {
-      var headers = credentials ? {
-        authorization: "Basic " + btoa(credentials.username + ":" + credentials.password)
-      } : {};
-
-      $http.get('user', {
-        headers: headers
-      }).success(function(data) {
-        if (data.name) {
-          $rootScope.authenticated = true;
-        }
-        else {
-          $rootScope.authenticated = false;
-        }
-        callback && callback();
-      }).error(function() {
-        $rootScope.authenticated = false;
-        callback && callback();
-      });
-
-    }
-
-    authenticate();
-    $scope.credentials = {};
-    $scope.login = function() {
-      authenticate($scope.credentials, function() {
-        if ($rootScope.authenticated) {
-          $location.path("/");
-          $scope.error = false;
-        }
-        else {
-          $location.path("/login");
-          $scope.error = true;
-        }
-      });
-    };
-
-    $scope.logout = function() {
-      $http.post('logout', {}).success(function() {
-        $rootScope.authenticated = false;
-        $location.path("/");
-      }).error(function(data) {
-        $rootScope.authenticated = false;
-      });
-    }
-  });
+});
--- user-management-client/src/test/java/com/szogibalu/untitled/usermanagement/client/UserManagementClientApplicationTests.java
+++ user-management-client/src/test/java/com/szogibalu/untitled/usermanagement/client/UserManagementClientApplicationTests.java
@@ -1,35 +1,15 @@
 package com.szogibalu.untitled.usermanagement.client;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import redis.embedded.RedisServer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = UserManagementClientApplication.class)
 @WebAppConfiguration
 public class UserManagementClientApplicationTests {
-
-    private static RedisServer redisServer;
-
-    @BeforeClass
-    public static void setup() throws Exception {
-        redisServer = new RedisServer(6379);
-        redisServer.start();
-    }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        try {
-            redisServer.stop();
-        } finally {
-            redisServer = null;
-        }
-    }
 
     @Test
     public void contextLoads() {
